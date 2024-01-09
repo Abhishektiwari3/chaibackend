f4106689a32fa8840e52b565c813102f8d0be761
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2024-01-10 01:37:27 +0530

    changes

---
+++ fullstack basic/backend/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('serve is ready')
+})
+
+// get a list of five jokes
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'This is a joke'
+        },
+
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'This is another joke'
+        },
+
+        {
+            id: 3,
+            title: 'third joke',
+            content: 'This is third joke'
+        },
+
+        {
+            id: 4,
+            title: 'fourth joke',
+            content: 'This is fourth joke'
+        },
+
+        {
+            id: 5,
+            title: 'fifth joke',
+            content: 'This is fifth joke'
+        }
+    ]
+    res.send(jokes)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
